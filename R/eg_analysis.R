ff4c15d322272b09fb15e9b0c468f79db82cfd29
David Jørgensen <36298489+JorgensenD@users.noreply.github.com>
2020-06-29 14:11:37 +0100

    r walkthroughs

---
+++ R/eg_analysis.R
@@ -0,0 +1,131 @@
+###########################################
+##                                       ##
+##      EG 2: Combine and summarize      ##
+##                                       ##
+###########################################
+
+#required packages
+require(sarscov2)
+require(ape)
+require(phangorn)
+require(ggplot2)
+require(ggtree)
+
+# Input some information for the plots and calculations
+location <- 'Madinah'
+lockdown_date <- NULL
+first_internal <- as.Date('2020-03-29')
+last_internal <- as.Date('2020-04-20')
+last_sequence <- as.Date('2020-05-16')
+
+# file path from the current working directory to the save location for the outputs
+# all path_to_save or ofn commands can be set to NULL to not save to file.
+path <- './eg_analysis/' 
+#dir.create(path)
+
+# we use multiple short chains and a large burnin to produce more rapid results. 
+burnin <- 50 #percentage
+
+# measure of lenience when including log and traj files, higher retains fewer. Setting to -1 will inlude all files
+pth <- 0.01
+
+# extract log and traj files produced from all of the beast runs by their file extentions 
+# If some traces show signs of stickiness or poor convergence they should be removed manually before this step
+logfns = list.files( pattern = '[0-9].xml.log$' )
+trajfns = list.files( pattern = '[0-9].xml.traj$' )
+
+
+# combine log and traj files and save each as .rds file
+combined <- combine_logs_and_traj(logfns, trajfns, burnProportion = burnin/100, ntraj=200, pth=pth,
+                                                       ofn = paste0(path,'logs.rds') , ofntraj = paste0(path,'traj.rds'))
+
+# produce a table of R0, growth rate and doubling time and save to object R_num 
+R_num <-  SEIJR_reproduction_number(paste0(path,'logs.rds'))
+
+# Import and format reported cases for comparison in plots.
+reported <- read.csv(paste0(path,'reportedcases.csv'), stringsAsFactors = F)
+reported$Date <- lubridate::parse_date_time(reported$Date.of.confirmation , c("Ymd", "dmY", "mdY"))
+reported$Date <- ymd(as.character(reported$Date))
+# Names required by functions
+colnames(reported) <- c('Date_char', 'Cumulative', 'Date')
+
+# calculate daily change from the daily cumulative cases.
+reported$Confirmed <- c(reported$Cumulative[1], diff(reported$Cumulative, lag=1))
+
+
+
+# plot of estimated and reported cumulative cases over time
+SEIJR_plot_size_returned <-  SEIJR_plot_size(trajdf = paste0(path,'traj.rds')
+                    , case_data = NULL
+                    , date_limits = c( first_internal, last_sequence ) 
+                    , path_to_save=paste0(path, 'size.png')
+                    , log_y_axis = T)
+SEIJR_plot_size_returned$pl
+
+# cumulative infections at last tip
+infections_at_last_tip <- SEIJR_plot_size_returned$pldf[which(
+  as.Date(SEIJR_plot_size_returned$pldf$Date) == last_internal &
+    SEIJR_plot_size_returned$pldf$reported == FALSE )        ,]
+
+
+# Plot of estimated and reported daily new infections 
+daily_inf <- SEIJR_plot_daily_inf( paste0(path,'traj.rds')
+                      , paste0(path,'logs.rds')
+                      , case_data = NULL
+                      , date_limits = c( first_internal, last_sequence ) 
+                      , path_to_save=paste0(path, 'daily_inf.png')
+                      , log_y_axis = T
+)
+daily_inf$pl
+
+# plot fo estimated Rt over time
+rt <- SEIJR_plot_Rt(paste0(path,'traj.rds')
+                   , paste0(path,'logs.rds')
+                   , gamma0 = 73
+                   , gamma1 = 121.667
+                   , date_limits = c(as.Date("2020-02-15"), NA)
+                   , path_to_save = paste0(path, 'Rt.png')
+)
+rt$plot
+
+
+# plot of proportion of total cases reported at each time point
+rep <- plot_rep(paste0(path,'traj.rds')
+                , case_data = reported
+                , date_limits = c(as.Date("2020-02-29"), NA)
+                , path_to_save = paste0(path,'reporting.png')
+                , errorbar = T)
+rep$pl
+
+## Producing and using the mcc tree
+
+
+treefiles <- list.files( pattern = '[0-9].xml.trees$')
+
+## select only the tree files for which log and traj files were retained
+included <- paste0('\\.',c(1,10,12,13,14,15,17,18,19,2,20,3,6,7,8), '\\.')
+
+treefiles <- treefiles[grep(paste(included, collapse="|"), treefiles)]
+
+# run these through BEAST2 functions - these are R wrappers for the existing commnad line functions. 
+# windows specific version is available for tree annotator as the command line interface differs. Check the package help files for this.
+tree_combiner_helper(burnin=burnin, fns=treefiles, ofn = paste0(path,'combined.trees'), resample = 10000)
+# On windows tree annotator needs to be run after using log cobiner as there is no -trees option
+tree_annotator_windows(inputfile = paste0(path,'combined.trees'), outputfile = paste0(path,'mcc.nex'), lowMem = T)
+
+# this nexus file can be used to plot the sampling distribution over time
+sarscov2:::plot_sample_distribution(path_to_nex = paste0(path,'mcc.nex'), path_to_save = paste0(path, 'sample_distribution.png'))
+
+
+# plotting the tree requires a table of continents for each of the included sequences.
+continents <- md[c('seq_location', 'Continent')]
+continents <- unique(continents)
+names(continents) <- c('name', 'continent')
+# Currently the function uses Americas rather than north and south america
+continents$continent <- gsub('NorthAmerica|SouthAmerica', 'Americas', continents$continent)
+# write to file
+write.table(country_dict, "country_dict.txt", col.names = T, row.names = F)
+
+mcc_col_tree_plot(paste0(path,'mcc.nex'), MRSD= last_sequence, "country_dict.txt", internal = location , style = 3, ofn=paste0(path,'mcc2.png'))
+
+
---
+++ R/eg_xml_format.R
@@ -0,0 +1,72 @@
+###########################################
+##                                       ##
+##            EG 1: BEAST2 XML           ##
+##                                       ##
+###########################################
+
+# load packages
+require(sarscov2)
+require(ape)
+require(phangorn)
+require(ggplot2)
+require(ggtree)
+
+
+# define some parameters 
+## path to the GISAID alignment: 
+fn = 'gisaid.fas' 
+## A simple name for the region:
+region='Madinah'
+## A path to the file containing small genetic distance pairs
+distfn = 'tn93dist.txt'
+## When do internal SEIR dynamics initiate: 
+startTime = 2020.085
+## What is the populatio size of the region of interest?
+popSize = 1100000
+## How many sequences to include within the region?
+n_region = 50
+## How many to include from the intertional reservoir?
+n_reservoir = 50 # (actual number to be included will be greater since it also includes close distance matches)
+## How many starting trees? A BEAST run will be carried out for each 
+n_startingtrees = 20 
+
+# Load the metadata from the ncov-gisaid repo
+md <- read.csv( 'md.csv', stringsAs=FALSE ) 
+
+# Set dedup = TRUE in region sampler and exog sampler to remove duplicate sequences
+
+# Let's sample from Madinah
+regiontips = region_sampler1( md, n = n_region  , inclusion_rules = list( c('RegionOrState', '^Madinah$') ), dedup = FALSE)
+
+# Alternatively you can include multiple inclusion criteria:
+# regiontips = region_sampler1( md, n = n_region
+#                               , inclusion_rules = 
+#                                 list( 
+#                                   c('CityOrCounty', '^KingCounty$')
+#                                   ,  c('CityOrCounty', '^Washington$')
+#                                 ) 
+# )
+# Sample a set of closely related external sequences, 
+exogtips = exog_sampler2( md, n=n_reservoir, smallGDpairs='tn93dist.txt', region_sample=regiontips, exclusion_rules = list( c('RegionOrState', '^Madinah$') ), dedup= FALSE )
+
+# You could select based on a different geographic criterion, current fields in metadata are  "Continent" "Country" "RegionOrState" "CityOrCounty"
+
+#~ Here is a subset of the metadata that corresponds to the sample. Check this carefully- is the sample including everything you want and not including things you dont want?
+mdregion = md[ match( regiontips, md$seqName ) , ]
+readline(prompt="At this point you should check the composition of regiontips and exogtips to make sure they match the sort of samples you want to include. Press a key to continue.")
+
+
+# Now make the aligment for BEAST. This adds sample time and deme (internal or) to tip labels:
+d3 = prep_tip_labels_seijr( 'gisaid.fas', outfn = 'algn3.fasta', plotout = 'MLtree.png', regiontips = regiontips, exogtips = exogtips, metadata = md  )
+
+
+# Make the starting trees
+# Note requires IQTREE
+tds = make_starting_trees('algn3.fasta', treeoutfn = "startTrees.nwk", ntres = n_startingtrees, ncpu = 6)
+
+file.copy( system.file( package='sarscov2', 'extdata/seijr0.1.3_skeleton.xml') , paste0(region, '.xml') )
+
+readline(prompt="At this point you should check the skeleton xml and make any necessary changes to prior distributions. You can also do this in beauti. Press any key to continue when this is done. ")
+
+# Make the xmls 
+format_xml0( xmlfn=paste0(region, '.xml'), fastafn = 'algn3.fasta', treefn='startTrees.nwk', start = startTime, susc_size = popSize) 
